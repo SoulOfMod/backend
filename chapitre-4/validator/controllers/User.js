16ef5c80189e06aae605dd1ee29952303e469367
Soul <albert.lucass@hotmail.com>
2021-06-16 17:01:01 +0200

    controller fait

---
+++ chapitre-4/validator/controllers/User.js
@@ -0,0 +1,76 @@
+const express = require("express")
+const router = express.Router()
+const User = require("./models/User")
+
+router.get("/users", async (req, res) => {
+    try {
+        const users = await User.find({})
+
+        res.json(users)
+    } catch (err) {
+        console.error(err)
+
+        res.json({ errorMessage: "There was a probleme :(" }, 500)
+    }
+
+})
+
+router.get("/users/:username", async (req, res) => {
+
+    const userGet = req.params
+    console.log(req.params);
+    console.log(req.params.username);
+    try {
+        const users = await User.find(userGet)
+
+        res.json(users)
+    } catch (err) {
+        console.error(err)
+
+        res.json({ errorMessage: "There was a probleme :(" }, 500)
+    }
+
+})
+
+router.get("/users/email/:email", async (req, res) => {
+
+    const userFound = req.params
+    console.log(req.params);
+    console.log(req.params.email);
+    try {
+        const users = await User.find(userFound)
+
+        res.json(users)
+    } catch (err) {
+        console.error(err)
+
+        res.json({ errorMessage: "There was a probleme :(" }, 500)
+    }
+
+})
+
+
+router.post('/users/add',
+    body("username").exists().isLength({ min: 4 }),
+    body("email").exists().isEmail(),
+    body("age").exists().isNumeric().isLength({ min: 2 }),
+    body("city").isIn(['Paris', 'Tokyo', 'Los Angeles']),
+    (req, res) => {
+        const errors = expressValidator.validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(500).json({
+                errors: errors.array(),
+                message: 'there is a problem'
+            });
+            return;
+        } else {
+            const user = req.body
+            const newUser = User.create(user)
+
+            res.json({
+                success: true,
+                message: 'User will be saved'
+            });
+        }
+    }
+);
--- chapitre-4/validator/server.js
+++ chapitre-4/validator/server.js
@@ -1,8 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
-// const usersRoutes = require("./controllers/user")
-const User = require("./models/User")
+const usersRoutes = require("./controllers/user")
 const { body } = require('express-validator');
 const expressValidator = require("express-validator");
 
@@ -22,81 +21,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// app.use("/users", usersRoutes)
+app.use("/users", usersRoutes)
 
 
-app.get("/users", async (req, res) => {
-    try {
-        const users = await User.find({})
-
-        res.json(users)
-    } catch (err) {
-        console.error(err)
-
-        res.json({ errorMessage: "There was a probleme :(" }, 500)
-    }
-
-})
-
-app.get("/users/:username", async (req, res) => {
-
-    const userGet = req.params
-    console.log(req.params);
-    console.log(req.params.username);
-    try {
-        const users = await User.find(userGet)
-
-        res.json(users)
-    } catch (err) {
-        console.error(err)
-
-        res.json({ errorMessage: "There was a probleme :(" }, 500)
-    }
-
-})
-
-app.get("/users/email/:email", async (req, res) => {
-
-    const userFound = req.params
-    console.log(req.params);
-    console.log(req.params.email);
-    try {
-        const users = await User.find(userFound)
-
-        res.json(users)
-    } catch (err) {
-        console.error(err)
-
-        res.json({ errorMessage: "There was a probleme :(" }, 500)
-    }
-
-})
-
-
-app.post('/users/add',
-    body("username").exists().isLength({ min: 4 }),
-    body("email").exists().isEmail(),
-    body("age").exists().isNumeric().isLength({ min: 2 }),
-    body("city").isIn(['Paris', 'Tokyo', 'Los Angeles']),
-    (req, res) => {
-        const errors = expressValidator.validationResult(req);
-        if (!errors.isEmpty()) {
-            res.status(500).json({
-                errors: errors.array(),
-                message: 'there is a problem'
-            });
-            return;
-        } else {
-            const user = req.body
-            const newUser = User.create(user)
-
-            res.json({
-                success: true,
-                message: 'User will be saved'
-            });
-        }
-    }
-);
 
 
 app.listen(port, () => {
